0710c269a16916ec717635822e69daf75ee0e0d0
Arkashka <108195123+ArkashkaGit@users.noreply.github.com>
2022-06-29 21:49:31 +0300

    second_commit

--- file.py
+++ file.py
@@ -1,12 +1,15 @@
 # список и функция
 
-list = [1,2,3,4,5,6,7,8,9]
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+list_2 = [10, 12, 13, 14, 15, 16, 17, 18, 19]
+
 
 def fun_1 (massiv):
     new_list = []
     for i in massiv:
-        if i > 4:
+        if i % 2 == 0:
             new_list += [i]
     print(new_list)
 
-fun_1(list)
+fun_1(list_1)
+fun_1(list_2)
